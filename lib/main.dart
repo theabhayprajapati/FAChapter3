bb18acf827c8d24fd872ccee7d55395a63c072ad
theabhayprajapati <theabhayprajapati@gmail.com>
2021-11-26 18:35:42 +0530

    working on chapter 3

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:html';
+
+import 'package:chapter2flutter/src/app.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Fooderich",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("FoodeRich"),
+          ),
+          body: const Text("Let's get cooking")),
+    );
+  }
+}
---
+++ lib/themes.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FooderichTheme {
+  static TextTheme lightTextTheme = TextTheme(
+    bodyText1: GoogleFonts.openSans(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+    headline1: GoogleFonts.openSans(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    headline2: GoogleFonts.openSans(
+      fontSize: 21.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    headline3: GoogleFonts.openSans(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+    headline6: GoogleFonts.openSans(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+  );  
+  
+}
